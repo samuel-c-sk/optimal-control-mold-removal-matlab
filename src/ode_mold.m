af8d1ada1d0bfee03cf0153b8477fe0b44ef7b08
samuel-c-sk <fphvsdfg94@privaterelay.appleid.com>
2026-01-30 12:51:32 +0100

    Update ode_mold.m

--- src/ode_mold.m
+++ src/ode_mold.m
@@ -1,1 +1,13 @@
+function dzdt = ode_mold(~, z, r, M, A)
+% ODE system for [x; p] under Pontryagin optimality condition.
 
+x = z(1);
+p = z(2);
+
+% Optimal control from dH/du = 0
+u = 0.5 * p * x;
+
+dzdt = zeros(2,1);
+dzdt(1) = r*(M - x) - u*x;          % state equation
+dzdt(2) = -2*A*x + p*(r + u);       % adjoint equation
+end
